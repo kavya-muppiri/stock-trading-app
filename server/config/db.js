e8637f30bffdc5ab0ac80a5725ad1ebcf7d734c7
muktheswarivipparla-prog <2023cse.r269@svce.edu.in>
2026-07-12 21:33:19 +0530

    Configure MongoDB Atlas connection and setup backend

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("URI:", process.env.MONGODB_URI);
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error("Database Connection Error:", error.message);
+    console.error("Full Error:");
+    console.error(error);
     process.exit(1);
   }
 };
